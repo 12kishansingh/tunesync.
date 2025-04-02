7953cd83ce28e3d7794163db3447a1b92e82dc2b
12kishansingh <ks888singh@gmail.com>
2025-04-02 14:01:09 +0530

    done with the ui of login page

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tunesync/components/bybutton.dart';
 import 'package:tunesync/components/mytextfiled.dart';
+import 'package:tunesync/components/squretile.dart';
 
 class LoginPage extends StatelessWidget {
-
-   LoginPage({super.key});
-  final usernamecontroller=TextEditingController();
-  final passwordcontroller=TextEditingController();
+  LoginPage({super.key});
+  final usernamecontroller = TextEditingController();
+  final passwordcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +15,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
                 height: 50,
@@ -46,14 +48,18 @@ class LoginPage extends StatelessWidget {
                 obscuretext: false,
               ),
 
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               //passowrd textfield
               Mytextfiled(
                 controller: passwordcontroller,
                 hinttext: 'Password',
                 obscuretext: true,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
 
               //forgot password
               Padding(
@@ -61,18 +67,86 @@ class LoginPage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text('Forgot Password?',
-                    style: TextStyle(color: Colors.grey[600]),),
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
                   ],
                 ),
               ),
-              //sign in button
+              const SizedBox(
+                height: 25,
+              ),
 
+              //sign in button
+              MyButton(
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 40,
+              ),
               // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: .5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text('Or Continue With'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: .5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
 
               // google sign in button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google
+                  SquareTile(imagepath: 'lib/assets/pic1.png'),
 
+                  // apple
+                  const SizedBox(
+                    width: 10,
+                  ),
+
+                  SquareTile(imagepath: 'lib/assets/pic3.png'),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
               // not a member , sign up!!!
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?'),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
